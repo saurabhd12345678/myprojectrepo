1927e5a8dd45f7ca4690400fd6f41f8a5554c66b
Saurabh Dhule <Saurabh Dhule@DESKTOP-MBA1FFR>
2021-03-23 09:49:39 +0530

    Added div and prod in master

--- SampleGitDemo/src/main/Calculator.java
+++ SampleGitDemo/src/main/Calculator.java
@@ -17,4 +17,11 @@ public class Calculator {
 	public static void produc(int num1,int num2){
 	      System.out.println(num1*num2);
 	  }
+	
+	public static void div(int num1,int num2){
+	      System.out.println(num1/num2);
+	  }
+	public static void mod(int num1,int num2){
+	      System.out.println(num1%num2);
+	  }
 }
