d0508aa1c70440f4ccf08109c1a8bdfa7dade454
Saurabh Dhule <Saurabh Dhule@DESKTOP-MBA1FFR>
2021-03-23 11:04:35 +0530

    five added in br

--- SampleGitDemo/src/main/Calculator.java
+++ SampleGitDemo/src/main/Calculator.java
@@ -36,4 +36,8 @@ public class Calculator {
 	public static void four(int num1){
 	      System.out.println(num1*num1*num1*num1);
 	  }
+	
+	public static void five(int num1){
+	      System.out.println(num1*num1*num1*num1*num1);
+	  }
 }
